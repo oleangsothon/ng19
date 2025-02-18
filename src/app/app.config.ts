26430b6f56b0c2a6d7ba13fd4cb9e8224bb6c89e
Sothon Sitsiwalai <sothon@odds.team>
2025-02-18 23:24:09 +0700

    🎨 feat: add log test provideAppInitializer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideAppInitializer, provideEnvironmentInitializer, providePlatformInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    providePlatformInitializer(() => {
+      console.log('Platform initialized');
+    }),
+    provideAppInitializer(() => {
+      console.log('App initialized');
+    }),
+    provideEnvironmentInitializer(() => {
+      console.log('Environment initialized');
+    })
+  ]
 };
